b7e97489b77f643224b6c45cfedaa94f1200a689
Charlie Liang Yuan <charlie.liang.yuan@gmail.com>
2017-08-06 14:11:27 -0700

    remove crap

--- scripts/drive.py
+++ scripts/drive.py
@@ -1,19 +1,16 @@
 """
 drive.py
 
-Script to run on the Raspberry PI to start your vehicle's drive loop. The drive loop
-will use post requests to the server specified in the remote argument. Run the
-serve.py script on a different computer to start the remote server.
+Script to run on the Raspberry PI to start your vehicle's drive loop. The
+drive loop will use post requests to the server specified in the remote
+argument. Run the serve.py script on a different computer to start the remote
+server.
 
 Usage:
-    drive.py [--remote=<name>] [--config=<name>]
+    drive.py
 
-Options:
-  --remote=<name>   recording session name
-  --config=<name>   vehicle configuration file name (without extension)  [default: vehicle]
 """
 
-import os
 import sys
 from docopt import docopt
 
@@ -24,18 +21,8 @@ args = docopt(__doc__)
 
 if __name__ == '__main__':
 
-    #load config file
+    # load config file
     cfg = dk.config.parse_config('~/mydonkey/' + args['--config'] + '.ini')
-
-    #get the url for the remote host (for user control)
-    remote_url = args['--remote']
-    if remote_url == None:
-        print('ERROR! : Must specify remote url')
-        sys.exit()
-    if not '//' in remote_url:
-        remote_url = 'http://'+remote_url
-    if not ':' in remote_url:
-        remote_url += ':8887'
 
     #load the actuators (default is the adafruit servo hat)
     mythrottlecontroller = dk.actuators.PCA9685_Controller(cfg['throttle_actuator_channel'])
@@ -54,21 +41,9 @@ if __name__ == '__main__':
     #abstract class to combine actuators
     mymixer = dk.mixers.AckermannSteeringMixer(mysteering, mythrottle)
 
-    #asych img capture from picamera
-    # mycamera = dk.sensors.PiVideoStream()
-
-    #setup the remote host
-    myremote = dk.remotes.RemoteClient(remote_url, vehicle_id=cfg['vehicle_id'])
-
-    #setup a local pilot
-    mypilot = dk.pilots.KerasCategorical(model_path=cfg['pilot_model_path'])
-    mypilot.load()
-
     #Create your car
     car = dk.vehicles.BaseVehicle(drive_loop_delay=cfg['vehicle_loop_delay'],
-                                  actuator_mixer=mymixer,
-                                  remote=myremote,
-                                  pilot=mypilot)
+                                  actuator_mixer=mymixer)
 
     #Start the drive loop
     car.start()
