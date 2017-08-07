1a2995b8051fa851c57b10b88dc629851dbd3e72
Ameesha <aisaac@lyft.com>
2017-08-06 17:45:09 -0700

    closest area

--- constants.py
+++ constants.py
@@ -3,7 +3,7 @@ class Constants:
     res_width=480
     frame_rate=50
     drive_loop_delay=0.5
-    closest_area=1200000
+    closest_area=1000000
     farthest_area=250000
     min_throttle=0.3
     max_throttle=0.4
